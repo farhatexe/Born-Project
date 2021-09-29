db2e082fbe534d6515ccb045a66ccb3c6bf8e225
MHmede <moh.hamadi98@gmail.com>
2021-09-29 10:11:18 +0300

    adding files

---
+++ Born-Project/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using Born_Project.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Born_Project.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly Born_ProjectDbContext _db;
+        public ProjectController(Born_ProjectDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Born-Project/Data/Born-ProjectDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Born_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Born_Project.Data
+{
+    public class Born_ProjectDbContext : DbContext
+    {
+        public Born_ProjectDbContext(DbContextOptions<Born_ProjectDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Project { get; set; }
+
+    }
+}
