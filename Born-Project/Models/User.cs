a892fdeb9a90fe8f79d98a85cdb22e91475046a2
MHmede <moh.hamadi98@gmail.com>
2021-09-30 17:36:47 +0300

    added last_name property to user model

--- Born-Project/Models/User.cs
+++ Born-Project/Models/User.cs
@@ -9,6 +9,7 @@ namespace Born_Project.Models
     {
         public int Id { get; set; }
         public string First_Name { get; set; }
+        public string Last_Name { get; set; }
         public string Email { get; set; }
 
         public string Address { get; set; }
